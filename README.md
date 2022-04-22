998014fd2bf2a8d6dfcd295789f142f72ea5444d
Jorge Rojo <jrojo@redpoints.com>
2022-04-22 22:49:49 +0200

    Initialize commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Red&King components
+
+A React components library for Red&King project.
+
+---
+## References
+- [TypeScript](https://www.typescriptlang.org/)
+- [Create React App](https://github.com/facebook/create-react-app)
+- [Storybook for React](https://github.com/storybookjs/storybook/tree/next/app/react)
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from '@emotion/styled-base';
+
+interface ButtonStyledProps {
+  size?: 'small' | 'medium' | 'large';
+  color?: 'primary' | 'secondary';
+}
+interface ButtonProps extends ButtonStyledProps {
+  label: string;
+  onClick?: () => void;
+}
+
+const styledPropHandler = {
+  color({ color }: ButtonStyledProps) {
+    switch (color) {
+      case 'secondary':
+        return `
+          color: #333;
+          background-color: transparent;
+          box-shadow: rgba(0, 0, 0, 0.25) 0px 0px 1px 2px inset;
+        `;
+      default:
+        return `
+          color: #fff;
+          background-color: #009179;
+        `;
+    }
+  },
+  size({ size }: ButtonStyledProps) {
+    switch (size) {
+      case 'small':
+        return `
+          font-size: 12px;
+          padding: 10px 16px;
+        `;
+      case 'large':
+        return `
+          font-size: 16px;
+          padding: 12px 24px;
+        `;
+      default:
+        return `
+          font-size: 14px;
+          padding: 11px 20px;
+        `;
+    }
+  },
+};
+
+export const ButtonStyled = styled('button')<ButtonStyledProps>`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 500;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+  ${styledPropHandler.color}
+  ${styledPropHandler.size}
+`;
+
+const Button = ({ label, size, color, onClick }: ButtonProps) => (
+  <ButtonStyled color={color} onClick={onClick} size={size} type="button">
+    {label}
+  </ButtonStyled>
+);
+
+export default Button;

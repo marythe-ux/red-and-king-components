114e05008c8e42687d24e1874de6e50a51925b81
marythe-ux <104101607+marythe-ux@users.noreply.github.com>
2022-04-23 15:25:29 +0200

    Create npm-publish.yml

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from '@emotion/styled-base';
+import styled from '@emotion/styled';
 
 interface ButtonStyledProps {
   size?: 'small' | 'medium' | 'large';

6c80252341d246b91fc602d17e3f8214f1bf9935
Jorge Rojo <jrojo@redpoints.com>
2022-05-09 01:30:40 +0200

    First design tokens usage

---
+++ src/theming/colors/Colors.tsx
@@ -0,0 +1,40 @@
+// import React from 'react';
+import styled from '@emotion/styled';
+import designTokens from '../../../tokens/design-tokens-example.json';
+// import css from '@emotion/css';
+
+const ColorCard = styled('span')`
+  width: 40px;
+  height: 40px;
+  display: inline-block;
+  margin: 2px;
+  background-color: ${({ color }) => color};
+  border: solid 1px #ccc;
+`;
+
+const printColor = (
+  colorName: string,
+  colorData: any
+): JSX.Element | JSX.Element[] => {
+  if (colorData.type === 'color') {
+    return (
+      <>
+        <ColorCard color={colorData.value} /> {colorName}
+      </>
+    );
+  }
+
+  return Object.entries(colorData).map(([name, value]) => (
+    <div>
+      <div>{printColor(`${colorName} ${name}`, value)}</div>
+    </div>
+  ));
+};
+
+const Colors = () => {
+  const colors = designTokens.color;
+
+  return <div>{printColor('', colors)}</div>;
+};
+
+export default Colors;

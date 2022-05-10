3cdf4c3ff6c4ba97dbf2ee165b785d396efc6db2
marythe-ux <104101607+marythe-ux@users.noreply.github.com>
2022-05-10 02:34:10 +0200

    Add support to Figma design tokens

--- .husky/run-not-in-branches.sh
+++ .husky/run-not-in-branches.sh
@@ -1,6 +1,6 @@
 branch=$(git branch --show-current)
 
-if [ $branch != "develop" && $branch != "main" && $branch != "gh-pages" ]; then
+if [ $branch != "develop" && $branch != "main" && $branch != "gh-pages" && $branch != "design-tokens" ]; then
     for command in "$@"; do
         $command
     done

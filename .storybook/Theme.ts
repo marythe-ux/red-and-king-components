be85eb3651dad35f74efd4b9eef880aa7e61a2b9
Jorge Rojo <jorge.rojodiseno@gmail.com>
2022-05-15 23:29:38 +0200

    Add secondary button border with token

---
+++ .storybook/Theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import logo from './public/topdone.svg';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Red&King components',
+  brandUrl: '/?path=/story/welcome-to-topdone--page',
+  brandImage: logo,
+});

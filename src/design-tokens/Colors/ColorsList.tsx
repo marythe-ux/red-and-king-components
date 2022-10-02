bd1592a633429942dabca9153a92a4aa3cb50495
Jorge Rojo <jorge.rojodiseno@gmail.com>
2022-10-02 18:03:21 +0200

    Preview action

--- src/design-tokens/Colors/ColorsList.tsx
+++ src/design-tokens/Colors/ColorsList.tsx
@@ -1,4 +1,4 @@
-import designTokens from 'red-and-king-components-tokens/dist/design-tokens.json';
+import designTokens from 'red-and-king-components-tokens/design-tokens.json';
 import {
   DesignTokensData,
   DesignTokensItem,
